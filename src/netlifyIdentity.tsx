d33a6ee04a56a7fd4119ee230313b1e031d282ec
NicolasPabloSoto <nicolas.diseno@gmail.com>
2024-09-23 20:50:51 -0300

    Megan Poll

---
+++ src/netlifyIdentity.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    netlifyIdentity: any;
+  }
+}
+
+const NetlifyIdentityWidget = () => {
+  useEffect(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", (user: any) => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  }, []);
+
+  return null;
+};
+
+export default NetlifyIdentityWidget;
